7c31375ef619336b4af988b826c6c232836bf6a2
Subham <subham.subham.sinharoy@gmail.com>
2021-07-11 14:20:42 +0530

    Added mantosh class and its functions

---
+++ src/main/java/algorithm/sorting/MantoshBaby.java
@@ -0,0 +1,19 @@
+package algorithm.sorting;
+
+public class MantoshBaby {
+    public static void main(String[] args) {
+        System.out.print("mantosh is bokachoda");
+    }
+
+    public void setup(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public void substract(){
+
+    }
+}
